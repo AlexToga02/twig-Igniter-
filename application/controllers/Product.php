8296be35fdd79f65832d0d5e32fe1cdc06fd0f32
Natanahel <neocid.natanahel@gmail.com>
2015-11-12 10:13:10 -0600

    Incio de sesion y registro
    falta los css

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -28,4 +28,70 @@ class Product extends CI_Controller {
 	{
 		$this->twig->display('index');
 	}
+	public function registrar()
+	{
+		$datos['nombre']=$this->input->post('name');
+		$datos['email']=$this->input->post('email');
+		$datos['user']=$this->input->post('username');
+		$datos['contrasena']=$this->input->post('passwd');
+		print_r($datos);
+	  //$this->db->insert('cliente',$datos);
+		$this->twig->display('index');
+		//$this->load->view('index.twig');
+		//$this->twig->render('index.twig');
+		//redirect();
+	}
+	/*-------------------------------------------------------Usuarios-------------------.-------------------------------------------*/
+
+		function login(){
+
+			$user =$this->input->post('cuenta');
+			print_r($user);
+			$contrasena =$this->input->post('clave');
+			print_r($contrasena);
+			$res = $this->validaUsuario($user,$contrasena);
+			print_r($res);
+			if(!empty($res)){
+				$datos = array(
+					'user'=>$res[0]['user'],
+					'nombre'=>$res[0]['nombre'],
+					'categoria'=>0
+				);
+				print_r($datos);
+				$this->session->set_userdata($datos);
+				$this->twig->display('login',$datos);
+
+			} else{redirect();}
+		}
+
+		function validaUsuario($user,$contrasena){
+			$this->db->where('user',$user);
+			$this->db->where('contrasena',$contrasena);
+
+			$query = $this->db->get('cliente');
+			return $query->result_array();
+		}
+
+		function logout(){
+		/*	$datos = array(
+				'cuenta'=>null,
+				'nombre'=>null,
+				'categoria'=>null
+			);
+			$this->session->set_userdata($datos);
+			$this->load->view('inicio');*/
+			$this->session->sess_destroy();
+			redirect();
+		}
+
+		function validaSession(){
+			if($this->session->userdata('cuenta')!=null)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+		}
 }
